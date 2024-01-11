2fec07586527b60d5087106afb096a2451f9317d
Yashu R <yashu.r@valtech.com>
2024-01-11 12:34:38 +0530

    updated seed command

--- seed.ps1
+++ seed.ps1
@@ -5,10 +5,10 @@ csdx auth:login
 #Import into Contentstack
 # $orgID = Read-Host "Enter Contentstack Organization ID"
 # $stackName = Read-Host "Enter new stack name"
-$repoPath = Read-Host "Enter git repo path - Organization/repo name"
+#$repoPath = Read-Host "Enter git repo path - Organization/repo name"
 # Write-Host $orgID + $stackName + $repoPath
 
-csdx cm:stacks:seed --repo $repoPath 
+csdx cm:stacks:seed --repo "Naga2478/ContentstackPOC" 
 # --org $orgID --stack-name $stackName
 
 # csdx cm:stacks:seed --repo "Naga2478/ContentstackPOC" --org "blt7d3ebf66c3f8e89e" --stack-name "POC-India"
