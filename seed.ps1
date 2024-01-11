d9cd12c19136e6fb7ee1d7f30122545a806c295c
Yashu R <yashu.r@valtech.com>
2024-01-11 12:28:52 +0530

    updating seed commands

--- seed.ps1
+++ seed.ps1
@@ -1,11 +1,14 @@
+Write-Host "You need to have admin/owner rights to import into existing stack or to create a new stack"
 #Login to Contentstack
 csdx auth:login
 
 #Import into Contentstack
-$orgID = Read-Host "Enter Contentstack Organization ID"
-$stackName = Read-Host "Enter new stack name"
-$repoPath = Read-Host "Enter git repo path"
+# $orgID = Read-Host "Enter Contentstack Organization ID"
+# $stackName = Read-Host "Enter new stack name"
+$repoPath = Read-Host "Enter git repo path - Organization/repo name"
+# Write-Host $orgID + $stackName + $repoPath
 
-Write-Host $orgID + $stackName + $repoPath
+csdx cm:stacks:seed --repo $repoPath 
+# --org $orgID --stack-name $stackName
 
-csdx cm:stacks:seed --repo $repoPath --org $orgID --stack-name $stackName
+# csdx cm:stacks:seed --repo "Naga2478/ContentstackPOC" --org "blt7d3ebf66c3f8e89e" --stack-name "POC-India"
